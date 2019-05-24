92319353b910e4f40feeb883eb3e820aecba5d6b
Shrinath <bshrinath@icloud.com>
2019-05-23 21:06:40 -0400

    Initial Commit

---
+++ PuglifeTests/PuglifeTests.swift
@@ -0,0 +1,34 @@
+//
+//  PuglifeTests.swift
+//  PuglifeTests
+//
+//  Created by Shrinath Badrinarayanan on 5/23/19.
+//  Copyright © 2019 Halcyon. All rights reserved.
+//
+
+import XCTest
+@testable import Puglife
+
+class PuglifeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
